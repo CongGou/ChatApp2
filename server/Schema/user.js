1e1f6efde6dbc3db913a6c2ccf9afa68c831be4f
JsPractice <1912433492@qq.com>
2020-01-04 22:27:42 +0800

    建立数据库用户表

--- server/Schema/user.js
+++ server/Schema/user.js
@@ -5,14 +5,18 @@ const userSchema = new mongoose.Schema({
   passWord: { type: String, required: true },
   introduce: { type: String, default: "这个人很懒什么都没有" },
   signature: { type: String, default: "这个人很懒什么都没有" },
-  photo: { type: String, default: "https://share.getcloudapp.com/4guxlYWD" },
+  photo: {
+    type: String,
+    default:
+      "https://p97.f4.n0.cdn.getcloudapp.com/items/KoueJ8Bb/136210.png?v=f9162ff7b4116eddc32e0edccb5fb9d5"
+  },
   date: { type: Date, default: Date.now() }
 });
 //密码加密 中间件
-const secret = "郭海聪";
+const secrets = "guohaicong";
 userSchema.pre("save", function(next) {
   let CryptoPassWord = crypto
-    .createHash("sha256", secret)
+    .createHash("sha256", secrets)
     .update(this.passWord)
     .digest("hex");
   this.passWord = CryptoPassWord;
